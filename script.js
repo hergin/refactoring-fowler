b6a6f8bc4d0e677b986a212f267ff8b2f97aac90
Ergin, Huseyin <hergin@bsu.edu>
2021-01-20 21:05:55 -0500

    MOVE to function: totalAmount

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 function statement(invoice, plays) {
   let result = `Statement for ${invoice.customer}\n`;
     
-  let totalAmount = 0;
   for (let perf of invoice.performances) {
     // print line for this order
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats)\n`;
-    totalAmount += amountFor(perf);
   }
-  result += `Amount owed is ${usd(totalAmount)}\n`;
+
+  result += `Amount owed is ${usd(totalAmount())}\n`;
 
   result += `You earned ${totalVolumeCredits()} credits\n`;
 
   return result;
+
+  function totalAmount() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += amountFor(perf);
+    }
+    return result;
+  }
 
   function totalVolumeCredits() {
     let result = 0;
