44ad3c17af05a766455783d91c30e9a3c3f7d7f3
Ergin, Huseyin <hergin@bsu.edu>
2021-01-20 21:16:51 -0500

    performance is enriched with amount

--- script.js
+++ script.js
@@ -7,11 +7,35 @@ function statement(invoice, plays) {
   function enrichPerformance(aPerformance) {
     const result = Object.assign({},aPerformance); // copy
     result.play = playFor(result);
+    result.amount = amountFor(result);
     return result;
   }
 
   function playFor(perf) {
     return plays[perf.playID];
+  }
+
+  function amountFor(aPerformance) {
+    let result = 0;
+
+    switch (aPerformance.play.type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${aPerformance.play.type}`);
+    }
+    return result;
   }
 }
 
@@ -20,7 +44,7 @@ function renderPlainText(data, plays) {
     
   for (let perf of data.performances) {
     // print line for this order
-    result += `  ${perf.play.name}: ${usd(amountFor(perf))} (${
+    result += `  ${perf.play.name}: ${usd(perf.amount)} (${
       perf.audience
     } seats)\n`;
   }
@@ -34,7 +58,7 @@ function renderPlainText(data, plays) {
   function totalAmount() {
     let result = 0;
     for (let perf of data.performances) {
-      result += amountFor(perf);
+      result += perf.amount;
     }
     return result;
   }
@@ -65,28 +89,6 @@ function renderPlainText(data, plays) {
     return result;
   }
 
-  function amountFor(aPerformance) {
-    let result = 0;
-
-    switch (aPerformance.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${aPerformance.play.type}`);
-    }
-    return result;
-  }
 }
 
 var invoices = [
